dfdda702bdfb087cefeef290badb770b8d586339
Filip Bozanovic <alymdrictels@gmail.com>
2016-02-16 00:41:13 +0100

    Add tnt.cpp

--- kolo2/tnt.cpp
+++ kolo2/tnt.cpp
@@ -5,9 +5,8 @@ int main()
   int cnt,kune,runde;
   cnt = scanf("%d %d", &kune, &runde);
 
-  for (int i = 0; i < runde && kune > 0; i++) {
-    kune -= runde + 1;
-  }
+  for (int i = 1; i < runde + 1; kune -= i++ + 1);
+  
   printf("%d", kune);
 
   return 0;
